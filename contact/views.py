38fa405ff1f488fd4fce2b8be8c2823193a22f42
GarethSRGKZY <divinegareth1313@gmail.com>
2024-07-05 15:27:18 +0800

    update GUI in views.py

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import (
+    QAbstractItemView,
     QWidget,
     QHBoxLayout,
     QMainWindow,
+    QPushButton,
+    QTableView,
+    QVBoxLayout,
 )
 
 class Window(QMainWindow):
@@ -15,3 +19,25 @@ class Window(QMainWindow):
         self.setCentralWidget(self.centralWidget)
         self.layout = QHBoxLayout()
         self.centralWidget.setLayout(self.layout)
+        self.setupUI()
+
+    def setupUI(self):
+        #Create table view widget
+        self.table = QTableView()
+        self.table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.table.resizeColumnsToContents()
+
+        #Create buttons
+        self.addButton = QPushButton("Add Contact")
+        self.deleteButton = QPushButton("Delete Contact")
+        self.clearAllButton = QPushButton("Clear All")
+
+        #GUI Layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.addButton)
+        layout.addWidget(self.deleteButton)
+        layout.addStretch()
+        layout.addWidget(self.clearAllButton)
+        self.layout.addWidget(self.table)
+        self.layout.addLayout(layout)
+        
