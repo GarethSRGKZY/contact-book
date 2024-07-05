96bf7d8f0eb5a87ecb08fe31f257987f92014286
GarethSRGKZY <divinegareth1313@gmail.com>
2024-07-05 15:51:39 +0800

    update main.py

--- contact/main.py
+++ contact/main.py
@@ -1,12 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from .database import createConnection
 from .views import Window
 
 def main():
     #Create application
     app = QApplication(sys.argv)
-    #Create main window
+    #Connect to database before windows are created
+    if not createConnection("contacts.sqlite"):
+        sys.exit(1)
+    #Create main window if connection succeeded
     win = Window()
     win.show()
     #Run application
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
