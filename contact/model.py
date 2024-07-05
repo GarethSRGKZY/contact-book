6c7ed089e84a1c03117ad127337948cd60329f17
GarethSRGKZY <divinegareth1313@gmail.com>
2024-07-05 16:08:56 +0800

    updated errors in model.py

--- contact/model.py
+++ contact/model.py
@@ -5,14 +5,14 @@ class ContactsModel:
     def __init__(self):
         self.model = self._createModel()
 
-        @staticmethod
-        def _createModel():
-            """Create and Set Up Model"""
-            model = QSqlTableModel()
-            model.setTable("contacts")
-            model.setEditStrategy(QSqlTableModel.OnFieldChange)
-            model.select()
-            headers = ("ID", "Name", "Job", "Email")
-            for columnIndex, header in enumerate(headers):
-                model.setHeaderData(columnIndex, Qt.Horizontal, header)
-            return model
+    @staticmethod
+    def _createModel():
+        """Create and Set Up Model"""
+        model = QSqlTableModel()
+        model.setTable("contacts")
+        model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        model.select()
+        headers = ("ID", "Name", "Job", "Email")
+        for columnIndex, header in enumerate(headers):
+            model.setHeaderData(columnIndex, Qt.Horizontal, header)
+        return model
