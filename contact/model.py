ac9569c4357e158199d8aecdd47d675e7de1fd8d
GarethSRGKZY <divinegareth1313@gmail.com>
2024-07-05 15:55:58 +0800

    create and update model.py

---
+++ contact/model.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtSql import QSqlTableModel
+
+class ContactsModel:
+    def __init__(self):
+        self.model = self._createModel()
+
+        @staticmethod
+        def _createModel():
+            """Create and Set Up Model"""
+            model = QSqlTableModel()
+            model.setTable("contacts")
+            model.setEditStrategy(QSqlTableModel.OnFieldChange)
+            model.select()
+            headers = ("ID", "Name", "Job", "Email")
+            for columnIndex, header in enumerate(headers):
+                model.setHeaderData(columnIndex, Qt.Horizontal, header)
+            return model
