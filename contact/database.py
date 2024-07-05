0e9334f6ca0d78178ab281a120a6620694491e61
GarethSRGKZY <divinegareth1313@gmail.com>
2024-07-05 15:51:23 +0800

    update database.py

--- contact/database.py
+++ contact/database.py
@@ -1,5 +1,19 @@
 from PyQt5.QtWidgets import QMessageBox
-from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtSql import QSqlDatabase, QSqlQuery
+
+def _createContactsTable():
+    """Create contacts table in database"""
+    createTableQuery = QSqlQuery()
+    return createTableQuery.exec(
+        """
+        CREATE TABLE IF NOT EXISTS contacts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL,
+            name VARCHAR(40) NOT NULL,
+            job VARCHAR(50),
+            email VARCHAR(40) NOT NULL,
+        )
+        """
+    )
 
 def createConnection(databaseName):
     """Create and open a database connection"""
@@ -13,4 +27,5 @@ def createConnection(databaseName):
             f"Database Error: {connection.lastError().text()}",
         )
         return False
+    _createContactsTable()
     return True
